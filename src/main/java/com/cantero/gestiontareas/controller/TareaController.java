2f3465426d654fefbc64c07389b8a2b4207753e5
cantero21 <cantero898@gmail.com>
2026-01-13 23:36:59 -0500

    cors

--- src/main/java/com/cantero/gestiontareas/controller/TareaController.java
+++ src/main/java/com/cantero/gestiontareas/controller/TareaController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tareas")
-@CrossOrigin("https://stalwart-paletas-d6b1a7.netlify.app/tareas")
+@CrossOrigin(origins = "https://stalwart-paletas-d6b1a7.netlify.app")
 public class TareaController {
 
     @Autowired
